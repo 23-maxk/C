786ad0cc6fb151ef06848a7d29342573552a2802
Lane <lanestokley23@yahoo.com>
2025-07-28 12:18:19 -0500

    Fix duplicate App components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,3 @@ import Customers from './components/Customers';
 import Jobs from './components/Jobs';
 import Invoices from './components/Invoices';
 import Estimates from './components/Estimates';
-
-function App() {
-  const [activeTab, setActiveTab] = useState('dashboard');
-
-  const renderContent = () => {
-    switch (activeTab) {
-      case 'dashboard':
-        return <Dashboard />;
-      case 'customers':
-        return <Customers />;
-      case 'jobs':
-        return <Jobs />;
-      case 'invoices':
-        return <Invoices />;
-      case 'estimates':
-        return <Estimates />;
-      default:
-        return <Dashboard />;
-    }
-  };
-
-  return (
-    <BrowserRouter>
-      <div className="flex min-h-screen bg-gray-50">
-        <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
-        <main className="flex-1 p-6">
-          {renderContent()}
-        </main>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-export default App;
